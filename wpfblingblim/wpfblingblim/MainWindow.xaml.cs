be22d6e860b9a7ff7495ccba8600b7f745145990
Waf888 <113286025+Waf888@users.noreply.github.com>
2025-04-01 15:30:30 +0200

    Add files via upload

---
+++ wpfblingblim/wpfblingblim/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfblingblim
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var rnd = new Random();
+
+            listView.Items.Add(new { m_nID = rnd.Next(), m_strName="Sebastian", m_strSname="Muzin", m_strPESEL="964576387345" });
+        }
+    }
+}
